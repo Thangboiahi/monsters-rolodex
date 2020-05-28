aa2f4d9afbbf5cb68bdd280639bc058bf3faf5b0
DD_THANG <thangboiahi@gmail.com>
2020-05-28 14:12:56 +0530

    Updates

---
+++ precache-manifest.62276ccc34ac6ffbdc036bed7d7e3466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afdf0ef369a0907a735b3f6d88a531b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2488ec8c14762a7623d3",
+    "url": "/monster-rolodex/static/css/main.bb2ed356.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2488ec8c14762a7623d3",
+    "url": "/monster-rolodex/static/js/main.1c4392a1.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
